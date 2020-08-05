3a35839f301ba00cbfe3e28ee255fa2bd375c488
yoneshinzato <chibariyo@gmail.com>
2020-08-04 23:38:39 -0300

    finaliza projeto

--- requisitions/src/App.js
+++ requisitions/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./App.css";
 
 //https://sujeitoprogramador.com/rn-api/?api=posts
 
@@ -11,25 +12,31 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://sujeitoprogramador.com/rn-api/?api=posts").then(
-      (response) => {
-        response.json().then((json) => {
-          let state = this.state;
-          state.nutri = json;
-          this.setState(state);
-          // console.log(json);
-        });
-      }
-    );
+    fetch("https://sujeitoprogramador.com/rn-api/?api=posts").then((r) => {
+      r.json().then((json) => {
+        let state = this.state;
+        state.nutri = json;
+        this.setState(state);
+        //console.log(json);
+      });
+    });
   }
 
   render() {
     return (
-      <div className="conttainer">
+      <div className="container">
+        <header>
+          <strong>React Nutri</strong>
+        </header>
         {this.state.nutri.map((item) => {
           return (
-            <article>
-              <strong> {item.titulo}</strong>
+            <article key={item.id} className="post">
+              <strong className="titulo">{item.titulo}</strong>
+              <img className="capa" src={item.capa} alt={item.titulo} />
+              <p className="subtitulo">{item.subtitulo}</p>
+              <a href="https://www.pudim.com.br" className="botao">
+                Veja mais
+              </a>
             </article>
           );
         })}
